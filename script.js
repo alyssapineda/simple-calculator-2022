e1236f3153f541f9f12077fb3c12626096d7ec4e
alyssa <110448688+alyssapineda@users.noreply.github.com>
2022-09-30 18:25:58 +1300

    js start

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const display = document.querySelector('.screen');
-const numbers = document.querySelectorAll('.')
+const numberBtn = document.querySelectorAll('[data-number]');
+const operationBtn = document.querySelectorAll('[data-operation]');
+const equalBtn = document.querySelector('[data-equal]');
+const allClearBtn = document.querySelector('[data-all-clear]');
+const ceBtn = document.querySelector('[data-delete]');
+
+const previousOp = document.querySelector('[data-prev-op]');
+const currentOp = document.querySelector('[data-current-op]');
