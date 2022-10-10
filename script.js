231e1d9aec13b4d1dae58e1b4a499a143a5d6c78
alyssa <110448688+alyssapineda@users.noreply.github.com>
2022-10-10 16:33:16 +1300

    under construction js not working

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-const display = document.querySelector('.screen');
-const numberBtn = document.querySelectorAll('[data-number]');
-const operationBtn = document.querySelectorAll('[data-operation]');
-const equalBtn = document.querySelector('[data-equal]');
-const allClearBtn = document.querySelector('[data-all-clear]');
-const ceBtn = document.querySelector('[data-delete]');
+// const screen = document.querySelector('.screen');
+// const numberBtn = document.querySelectorAll('[data-number]');
+// const operationBtn = document.querySelectorAll('[data-operation]');
+// const equalBtn = document.querySelector('[data-equal]');
+// const allClear = document.querySelector('[data-all-clear]');
+// const delBtn = document.querySelector('[data-delete]');
 
-const previousOp = document.querySelector('[data-prev-op]');
-const currentOp = document.querySelector('[data-current-op]');
+// const previousOp = document.querySelector('[data-prev-op]');
+// const currentOp = document.querySelector('[data-current-op]');
+
+const screenel = document.querySelector('[data-screen]')
+
+function display(number){
+  document.querySelector('#result').value += number;
+  screenel.textContent = number;
+  return number;
+}
+  
+function calc(){
+  let x = document.querySelector('#result').value;
+  let y = eval(x);
+  document.querySelector('#result').value = y;
+  screenel.textContent = y;
+  return y
+}
+
+function clear(){
+  document.querySelector('#result').value = ''
+}
