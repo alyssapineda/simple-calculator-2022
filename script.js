22dc65870ef1cfbdc6cba6e1f17d5b02adddd3d9
alyssa <110448688+alyssapineda@users.noreply.github.com>
2022-10-17 19:02:07 +1300

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,53 @@
-let screenel = document.querySelector('#result');
-let calculation = "";
-let result = "";
-let lastOperator = "";
-let newOperator = "";
+let equal_pressed = 0;
+let button_input = document.querySelectorAll(".input-button");
 
+//refer input, equal, clear and erase 
+let input = document.querySelector("#input");
+let clear = document.querySelector("#clear");
+let erase = document.querySelector("#erase");
+let equal = document.querySelector("#equal");
 
-function display(value){
+window.onload = () => {
+  input.value = "";
+};
 
-	if(value == '%')
-	{
-		lastOperator = "%";
-	}else if(value == '*'){
-		lastOperator = "*";
-	}else if (value == '-'){
-		lastOperator = "-";
-	}else if (value == '/'){
-		lastOperator = "/";
-	}else if (value == '+'){
-		lastOperator = "+";
-	}
-
-	console.log("The Value is " + value);
-	console.log("The lastOperator is " + lastOperator);
-
-	if(lastOperator == "" || (newOperator != lastOperator) || (value != lastOperator))
-	{
-		result = result.concat(calculation, value);
-    	screenel.textContent = result;
-    	newOperator = lastOperator;
-    	console.log("New Operator" + newOperator);
-    	return;
+//access each class using forEach
+button_input.forEach((button_class) => {
+  button_class.addEventListener("click", () => {
+    if (equal_pressed == 1) {
+      input.value = "";
+      equal_pressed = 0;
     }
-}
-  
-function calc(){
-  let y = eval(result);
-  screenel.textContent = String(y);
-}
+    //display value of each button
+    input.value += button_class.value;
+  });
+});
 
-function clear(){
-  screenel.textContent = "";
-  calculation = "";
-  result = "";
-  return;
-}
+//solve user's input when equal button is clicked
+equal.addEventListener("click", () => {
+  equal_pressed = 1;
+  let inp_val = input.value;
+  try {
+    //evaluate user's input
+    let result = eval(inp_val);
+    //true for natural numbers
+    //false for decimals
+    if(Number.isInteger(result)) {
+      input.value = result;
+    }else {
+      input.value = result.toFixed(2);
+    } 
+  } catch(error) {
+      alert("Please click on valid input button!");
+  }
+});
+
+// AC button deletes entire input on screen
+clear.addEventListener("click", () => {
+  input.value = "";
+});
+
+//DEL button deletes single item
+erase.addEventListener("click", () => {
+  input.value = input.value.substr(0,input.value.length - 1);
+});
